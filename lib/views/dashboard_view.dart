a4c92a0493862621c7a4e743b98684f4e58f6b46
marco <119810706+array-marco@users.noreply.github.com>
2024-05-02 15:21:00 +0300

    add text styles

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout_widget.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const SizedBox()),
+    );
+  }
+}
---
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashBoardLayout extends StatelessWidget {
+  const DashBoardLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
