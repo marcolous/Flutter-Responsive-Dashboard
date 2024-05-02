d5c0f243bd25604b05062a4ef33f7bef02af7e1a
marco <119810706+array-marco@users.noreply.github.com>
2024-05-02 15:47:22 +0300

    add assests

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,55 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imagesCardReceive
+  /// assests/images/card-receive.svg
+  static const String imagesCardReceive = "assests/images/card-receive.svg";
+
+  /// Assets for imagesCardSend
+  /// assests/images/card-send.svg
+  static const String imagesCardSend = "assests/images/card-send.svg";
+
+  /// Assets for imagesDashboard
+  /// assests/images/dashboard.svg
+  static const String imagesDashboard = "assests/images/dashboard.svg";
+
+  /// Assets for imagesFrame
+  /// assests/images/Frame.svg
+  static const String imagesFrame = "assests/images/Frame.svg";
+
+  /// Assets for imagesFrame2
+  /// assests/images/Frame2.svg
+  static const String imagesFrame2 = "assests/images/Frame2.svg";
+
+  /// Assets for imagesLogout
+  /// assests/images/logout.svg
+  static const String imagesLogout = "assests/images/logout.svg";
+
+  /// Assets for imagesMoneys
+  /// assests/images/moneys.svg
+  static const String imagesMoneys = "assests/images/moneys.svg";
+
+  /// Assets for imagesMyCard
+  /// assests/images/my-card.svg
+  static const String imagesMyCard = "assests/images/my-card.svg";
+
+  /// Assets for imagesMyInvestments
+  /// assests/images/my-investments.svg
+  static const String imagesMyInvestments = "assests/images/my-investments.svg";
+
+  /// Assets for imagesMyTransaction
+  /// assests/images/my-transaction.svg
+  static const String imagesMyTransaction = "assests/images/my-transaction.svg";
+
+  /// Assets for imagesSettings
+  /// assests/images/settings.svg
+  static const String imagesSettings = "assests/images/settings.svg";
+
+  /// Assets for imagesStatistics
+  /// assests/images/statistics.svg
+  static const String imagesStatistics = "assests/images/statistics.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assests/images/wallet-account.svg
+  static const String imagesWalletAccount = "assests/images/wallet-account.svg";
+}
