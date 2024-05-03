d5e2292ed48e83f4083845b57329adbb301de5db
marco <119810706+array-marco@users.noreply.github.com>
2024-05-03 12:53:59 +0300

    finish drawer list

--- lib/widgets/buttom_drawer_listview.dart
+++ lib/widgets/buttom_drawer_listview.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/models/drawe_item_model.dart';
 import 'package:responsive_dashboard/widgets/drawer_item.dart';
 
-class DrawerItemListView extends StatefulWidget {
-  const DrawerItemListView({super.key, required this.draweItem});
+class ButtomDrawerListview extends StatefulWidget {
+  const ButtomDrawerListview({super.key, required this.draweItem});
 
   final List<DrawerItemModel> draweItem;
 
   @override
-  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+  State<ButtomDrawerListview> createState() => _ButtomDrawerListviewState();
 }
 
-class _DrawerItemListViewState extends State<DrawerItemListView> {
+class _ButtomDrawerListviewState extends State<ButtomDrawerListview> {
   int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return SliverList.builder(
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       itemCount: widget.draweItem.length,
       itemBuilder: (context, index) {
         return GestureDetector(
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:responsive_dashboard/models/drawe_item_model.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/drawer_item.dart';
 import 'package:responsive_dashboard/widgets/drawer_item_listview.dart';
 import 'package:responsive_dashboard/widgets/user_info_list_tile.dart';
 
@@ -16,25 +19,44 @@ class CustomDrawer extends StatelessWidget {
     DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments'),
   ];
 
-  // static const List<DraweItemModel> draweItemBottom = [
-  //   DraweItemModel(image: Assets.imagesSettings, title: 'Setting system'),
-  //   DraweItemModel(image: Assets.imagesLogout, title: 'Logout account'),
-  // ];
+  static const List<DrawerItemModel> draweItemBottom = [
+    DrawerItemModel(image: Assets.imagesSettings, title: 'Setting system'),
+    DrawerItemModel(image: Assets.imagesLogout, title: 'Logout account'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        UserInfoListTile(
-          image: Assets.imagesFrame3,
-          title: 'Lekan Okeowo',
-          subTitle: 'demo@gmail.com',
-        ),
-        DrawerItemListView(draweItem: draweItem),
-        Expanded(child: Gap(20)),
-        //DrawerItemListView(draweItem: draweItemBottom),
-        Gap(48)
-      ],
+    return Container(
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              image: Assets.imagesFrame3,
+              title: 'Lekan Okeowo',
+              subTitle: 'demo@gmail.com',
+            ),
+          ),
+          DrawerItemListView(draweItem: draweItem),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox(height: 10)),
+                //ButtomDrawerListview(draweItem: draweItemBottom),
+                DrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                        title: 'Settings', image: Assets.imagesSettings),
+                    isActive: false),
+                DrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                        title: 'Logout', image: Assets.imagesLogout),
+                    isActive: false),
+                Gap(48)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
